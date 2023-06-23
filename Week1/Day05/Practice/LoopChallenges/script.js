3fc3984d4a7d9e5664a046079377e965abbc7cae
unknown <khalilmeddeb123@gmail.com>
2023-06-23 17:19:17 +0100

    Practice-Fizz-Loop

---
+++ Week1/Day05/Practice/LoopChallenges/script.js
@@ -0,0 +1,32 @@
+for (let a = 1; a < 21 ; a++) {
+   if (a % 2 != 0) {
+    console.log(a);
+   }
+}
+
+for (let y = 100; y >=0 ; y--) {
+      if (y% 3 == 0 ) {
+        console.log(y);
+      } 
+}
+
+
+for (let i = 5.5 ; i > -4; i--) {
+     i = i -1.5;
+     console.log(i);    
+}
+
+var sum = 0 ;
+for (let j = 1 ; j < 101 ; j++) {
+     sum += j ;
+     sum + j ;
+     console.log(sum);
+}
+
+var mult = 1; 
+for (var x = 1; x < 13 ; x++); {
+    mult *= x;
+    mult + x ;
+    console.log(mult);
+
+}
